0c633a2b682ff2e5d47dacd41122ff42fe1111c7
mattgod56 <matthieu.godme@epitech.eu>
2024-05-08 15:30:32 +0200

    feat(Parsing): added a vector for cube to be able to rotate

--- src/Shapes/Cube.hpp
+++ src/Shapes/Cube.hpp
@@ -14,13 +14,14 @@
 namespace RayTracer {
 class Cube : public AShape {
 public:
-    Cube(const Point3D &center, double size, const Material &material)
-        : AShape(center, material), m_size(size) {}
+    Cube(const Point3D &center, double size, const Vector3D &axis, const Material &material)
+        : AShape(center, material), m_size(size), m_vect(axis) {}
 
     virtual bool hits(const Ray &ray, Math::RGBA &hitColor, double &t) const override;
     virtual Vector3D getNormal(const Point3D &hitPoint) const override;
 
 private:
     double m_size;
+    Vector3D m_vect;
 };
 } // namespace RayTracer
